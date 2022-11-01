4fe1523ed3332d5e2047759c83b63b42f1f7ddc2
randomtwdude <105645765+randomtwdude@users.noreply.github.com>
2022-11-01 14:26:05 +0800

    Fixed compile

---
+++ functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include "struct.h"
+
+void createPlayer(player player1, player player2);
+
+void addCard(player p, card c);
+
+void dealCard(player p, int num);
+
+void gameLoop(player player1, player player2);
+
+void createCards();
+
+#endif
